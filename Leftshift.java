398597ea4958eb28bbc017537fee700d627b18d1
Pankaj12kumbh <94217321+Pankaj12kumbh@users.noreply.github.com>
2022-01-20 08:33:56 +0530

    Add files via upload
    The Java left shift operator << is used to shift all of the bits in a value to the left side of a specified number of times

---
+++ Leftshift.java
@@ -0,0 +1,8 @@
+public class Leftshift {
+    public static void main(String[] args){
+        System.out.println(10<<2); //10*2^2
+        System.out.println(10<<3); //10*2^3
+        System.out.println(20<<2); //20*2^2
+        System.out.println(15<<4); //15*2^4
+    }
+}
