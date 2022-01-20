307731608c8b5a3dd0ed0369bc074e975aa9e24e
Pankaj12kumbh <94217321+Pankaj12kumbh@users.noreply.github.com>
2022-01-20 08:31:43 +0530

    Add files via upload
    The Java unary operators require only one operand. Unary operators are used to perform various operations i.e.:
    
    
    incrementing/decrementing a value by one
    negating an expression
    inverting the value of a boolean

---
+++ Unary2.java
@@ -0,0 +1,9 @@
+public class Unary2 {
+    public static void main(String[] args){
+        int a=10;
+        int b=10;
+        System.out.println(a++ + ++a); //10(11)+12
+        System.out.println(b++ + b++); //10+11
+
+    }
+}
